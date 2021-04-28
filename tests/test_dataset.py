d56171479c0c8e054027fd91bc92d527705f68cc
bipin <bipinkrishna.p@gmail.com>
2021-04-28 11:15:03 +0530

    added more tests for dataset preparation

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,13 +1,21 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
-from src import create_dataset
+from src import create_dataset, utils
 from pandas import DataFrame
+import pytest
 
-def test_data_preparation():
+@pytest.fixture
+def get_config():
+    config = utils.open_yaml("params.yaml")
+    return config
+
+def test_data_preparation(get_config):
     df = create_dataset.create_ds("params.yaml")
+    ds_path = get_config['data']["dataset_path"].split("/")[-1]
 
     assert type(df)== DataFrame
+    assert (ds_path in os.listdir("data"))==True
 
 
 
